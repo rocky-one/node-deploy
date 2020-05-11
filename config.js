2eedfbec712c78ccccb027974013effe4c1a8da7
rocky-one <jiangjinlongyx@163.com>
2020-05-11 22:39:04 +0800

    修改配置文件

--- config.js
+++ config.js
@@ -2,11 +2,11 @@
 const config = {
   ssh: [{
     // 服务器IP
-    host: '39.105.117.48',
+    host: '39.105.123.123',
     // 端口
     port: 22,
     username: 'root',
-    password: 'smile..12',
+    password: '123456',
     // 部署到服务器的哪个目录
     deployDir: '/root/mdd/web/',
     // 相当于是/root/mdd/web/build, build为了重命名解压出来的文件夹名称
